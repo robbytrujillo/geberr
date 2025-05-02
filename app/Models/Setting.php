585eeca893acea66d3c4cf71468c7a83dcf2fd9b
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-02 11:38:51 +0700

    Create Model, Migration and Resouce Setting | BE Geberr App

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    protected $fillable = [
+        'interval_seconds',
+        'price_per_km'
+    ];
+
+    // mendefiniskan variable
+    protected $casts = [
+        'interval_seconds' => 'integer',
+        'price_per_km' => 'decimal:2'
+    ];
+
+    protected static function boot () {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (static::count() > 0) {
+                return false;
+            }
+        });
+    }
+
+    public static function getSettings() {
+        return static::first();
+    }
 }
