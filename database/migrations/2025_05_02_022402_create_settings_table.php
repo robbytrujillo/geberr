c77128c67c9e7927dc2e0e4915d02a33ee14a41b
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-02 09:30:28 +0700

    Create Model, Migration and Resouce Setting | BE Geberr App

---
+++ database/migrations/2025_05_02_022402_create_settings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('interval_seconds')->comment('Interval waktu dalam detik');
+            $table->decimal('price_per_km', 10, 2)->comment('Harga per km');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
