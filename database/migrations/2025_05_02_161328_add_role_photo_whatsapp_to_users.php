3e0e89ee167e8488a14a3001d86e0464c86d529e
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-03 06:07:29 +0700

    Add Column To User Table & Create User Resource | BE Geberr App

--- database/migrations/2025_05_02_161328_add_role_photo_whatsapp_to_users.php
+++ database/migrations/2025_05_02_161328_add_role_photo_whatsapp_to_users.php
@@ -25,7 +25,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
+            $table->dropColumn('photo');
+            $table->dropColumn('whatsapp');
+            $table->dropSoftDeletes();
+
         });
     }
 };
