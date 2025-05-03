3bab1004333665fb4f28eae90373d2c15e8fa034
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-04 04:16:22 +0700

    Create Model, Migration, Resource Driver | BE Geberr App

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Driver extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'vehicle_number',
+        'is_active',
+        'current_latitude',
+        'current_longitude',
+        'last_online'
+    ];
 }
