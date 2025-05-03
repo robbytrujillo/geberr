39e63a709248837df1018f9b85dd32a6312008b2
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-03 20:01:22 +0700

    Create Model, Migration, Resource Driver | BE Geberr App

---
+++ database/migrations/2025_05_03_125044_create_drivers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('vehicle_number')->unique();
+            $table->boolean('is_active')->default('true');
+            $table->decimal('current_latitude', 10, 6)->nullable();
+            $table->decimal('current_longitude', 10, 6)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drivers');
+    }
+};
