38e9b4a4490bf33ba93951d127b5de556dc33d14
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-04 19:55:22 +0700

    Create Booking Revision Migration | BE Geberr App

--- database/migrations/2025_05_04_123816_create_bookings_table.php
+++ database/migrations/2025_05_04_123816_create_bookings_table.php
@@ -14,7 +14,23 @@ return new class extends Migration
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id')->constrained('users')->cascadeOnDelete();
-            $table->foreignId('driver_id')->nullable()->constrained('users')->cascadeOnDelete();
+            $table->foreignId('driver_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->decimal('latitude_origin', 10, 8);
+            $table->decimal('longitude_origin', 10, 8);
+            $table->string('address_origin');
+            $table->decimal('latitude_destination', 10, 8);
+            $table->decimal('longitude_destination', 10, 8);
+            $table->string('address_destination');
+            $table->decimal('distance', 10, 2);
+            $table->decimal('price', 10, 2);
+            $table->enum('status', [
+                'finding_driver',
+                'driver_pickup',
+                'driver_deliver',
+                'arrived',
+                'paid',
+                'cancelled',
+            ])->default('finding_driver');
             $table->timestamps();
         });
     }
