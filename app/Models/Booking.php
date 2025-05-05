45782a4a93a49c59c91d8fb25091da73281a13b8
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-05 07:46:29 +0700

    Creating Model & Resource Booking  Revisions | BE Geberr App

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Booking extends Model
 {
-    //
+    use SoftDeletes;
+
+    const STATUS_FIDING_DRIVER = 'finding_driver';
+    const STATUS_DRIVER_PICKUP = 'driver_pickup';
+    const STATUS_DRIVER_DELIVER = 'driver_deliver';
+    const STATUS_ARRIVED = 'arrived';
+    const STATUS_PAID = 'paid';
+    const STATUS_CANCELED = 'cancelled';
+
+    protected $fillable = [
+        'costomer_id',
+        'driver_id',
+        'latitude_origin',
+        'longitude_origin',
+        'address_origin',
+        'latitude_destination',
+        'longitude_destination',
+        'address_destination',
+        'distance',
+        'price',
+        'status',
+    ];
 }
