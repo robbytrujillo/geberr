5c07e4b344bd47459a9d1436baca42e49cca0586
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-05 11:53:57 +0700

    Creating View Tab for User Table expect role | BE Geberr App

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListUsers extends ListRecords
 {
@@ -14,6 +17,26 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array {
+        return [
+            'admin' => Tab::make('Admins')
+                    ->icon('heroicon-o-shield-check')
+                    ->badge(fn () => User::where('role', 'admin')->count())
+                    ->modifyQueryUsing(fn (Builder $query)=> $query->where('role', 'admin')),
+            'customer' => Tab::make('Customers')
+                    ->icon('heroicon-s-user-group')
+                    ->badge(fn () => User::where('role', 'customer')->count())
+                    ->modifyQueryUsing(fn (Builder $query)=> $query->where('role', 'customer')),
+            'driver' => Tab::make('Drivers')
+                    ->icon('heroicon-o-truck')
+                    ->badge(fn () => User::where('role', 'driver')->count())
+                    ->modifyQueryUsing(fn (Builder $query)=> $query->where('role', 'driver')),
+
+                  
+                    
         ];
     }
 }
