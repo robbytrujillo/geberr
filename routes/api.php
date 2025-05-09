11edab65ede45bb321426544f60f37924bef12fc
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-09 08:50:42 +0700

    Create API Login

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 
-Route::get('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
