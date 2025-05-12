b5012c4234efa2a9497c3a2b338b40a0ac27f428
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-12 13:12:34 +0700

    Tidy up the appearance of the booking table

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('booking')->group(function () {
         Route::post('price-check', [BookingController::class, 'priceCheck']);
         Route::post('/', [BookingController::class, 'store'])->name('booking');
+        Route::post('/{booking}/cancel', [BookingController::class, 'cancel'])->name('booking.cancel');
     });
 });
 
