f311c3e92e4b977282f70a20779deeed8cbd642a
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-19 11:25:38 +0700

    Creating Get API data driver tracking saved

--- app/Http/Controllers/API/DriverTrackingController.php
+++ app/Http/Controllers/API/DriverTrackingController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use App\Models\DriverTracking;
 
 class DriverTrackingController extends Controller
 {
     // Booking tracking
     public function store(Request $request) {
-        $validate = $request->validate([
+        if (!auth()->user()->checkDriver()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Akun Anda bukan driver',
+                'data' => auth()->user()
+            ], 403);
+        }
+
+        $validator = Validator::make($request->all(), [
             /**
              * @example -6.313131 description
              */
@@ -20,12 +31,12 @@ class DriverTrackingController extends Controller
             'longitude' => 'required|numeric|between:-180,180',
         ]);
 
-        if (!auth()->user()->checkDriver()) {
+        if ($validator->fails()) {
             return response()->json([
                 'success' => false,
-                'message' => 'Akun Anda bukan driver',
-                'data' => auth()->user()
-            ], 403);
+                'message' => 'Validation Error',
+                'data' => ['errors' => $validator->errors()]
+            ], 422);
         }
 
         $timestamp = now();
@@ -35,6 +46,18 @@ class DriverTrackingController extends Controller
             'latitude' => $request->latitude,
             'longitude' => $request->longitude,
             'tracked_at' => $timestamp,
+        ]);
+
+        $request->user()->driver->update([
+            'current_latitude' => $request->latitude,
+            'current_longitude' => $request->longitude,
+            'last_online' => $timestamp
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data berhasil disimpan',
+            'data' =>   $driverTracking
         ]);
     }
 }
