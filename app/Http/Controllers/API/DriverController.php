95661d9bc3dc4f007499d49e5f2afddeeba2991c
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-30 05:52:34 +0700

    Create an API to set active or inactive Driver Account

---
+++ app/Http/Controllers/API/DriverController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DriverController extends Controller
+{
+    public function toggleActive() {
+        if (!auth()->user()->checkDriver()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Akun Anda bukan driver',
+                'data' => null
+            ], 403);
+        }
+
+        $driver = Driver::where('user_id', auth()->user()->id)->first();
+        if (!$driver) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data Driver tidak ditemukan',
+                'data' => null
+            ], 404);
+        }
+
+        $activeBooking = Booking::getActiveBooking(auth()->user()->id, 'driver', auth()->user()->driver->id);
+        if ($activeBooking) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Anda masih memiliki booking aktif',
+                'data' => null
+            ], 422);
+        }
+    }
+}
